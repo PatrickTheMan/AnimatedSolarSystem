487e0f9050df7605dab2ba24d988d9366fa44098
Patrick Schemel <Patrick@schemel.dk>
2022-10-06 12:35:05 +0200

    1 Works

--- AnimatedSolarSystem/View/SolarSystemView.xaml.cs
+++ AnimatedSolarSystem/View/SolarSystemView.xaml.cs
@@ -25,58 +25,11 @@ namespace AnimatedSolarSystem.View
             this.DataContext = SolarSystemViewModel;
 		}
 
-        SolarObject Mercury = new SolarObject("Mercury", 75 + 50, 0.479, 10, Brushes.Gray);
-        SolarObject Venus = new SolarObject("Venus", 100 + 50, 0.350, 20, Brushes.LightGoldenrodYellow);
-        SolarObject Earth = new SolarObject("Earth", 130 + 50, 0.298, 20, Brushes.Blue);
-        SolarObject Mars = new SolarObject("Mars", 155 + 50, 0.241, 10, Brushes.OrangeRed);
-		SolarObject Jupiter = new SolarObject("Jupiter", 305 + 50, 0.131, 100, Brushes.Wheat);
-		SolarObject Saturn = new SolarObject("Saturn", 415 + 50, 0.97, 80, Brushes.SandyBrown);
-		SolarObject Uranus = new SolarObject("Uranus", 505 + 50, 0.68, 50, Brushes.WhiteSmoke);
-		SolarObject Neptun = new SolarObject("Neptun", 590 + 50, 0.54, 50, Brushes.LightBlue);
-
-        List<SolarObject> SolarSystem;
+        
         private void Create_Btn_Click(object sender, RoutedEventArgs e)
         {
-			this.SolarSystem = new List<SolarObject>();
-			this.SolarSystemViewModel.NewCanvas();
-			this.CControl_Canvas.Content = SolarSystemViewModel.Canvas;
-
-
-			Ellipse Sun = new Ellipse();
-            Sun.Fill = Brushes.Yellow;
-			Sun.Width = 200;
-			Sun.Height = 200;
-			Sun.Margin = new Thickness(707, 307, 0, 0);
-			Sun.StrokeThickness = 2;
-
-			Sun.Stroke = Brushes.White;
-
-			SolarSystemViewModel.Canvas.Children.Add(Sun);
-
-
-			SolarSystem.Add(Mercury);
-			SolarSystem.Add(Venus);
-			SolarSystem.Add(Earth);
-			SolarSystem.Add(Mars);
-			SolarSystem.Add(Jupiter);
-			SolarSystem.Add(Saturn);
-			SolarSystem.Add(Uranus);
-			SolarSystem.Add(Neptun);
-
-
-			Canvas.SetZIndex(Sun, 0);
-
-			int count = 1;
-			foreach (SolarObject planet in SolarSystem)
-            {
-				Canvas.SetZIndex(planet.Shape, count);
-				count++;
-
-                Debug.WriteLine("Count: "+count);
-
-				SolarSystemViewModel.Canvas.Children.Add(planet.Shape);
-			}
-
+            SolarSystemViewModel.CreateSolarSystem();
+            this.CControl_Canvas.Content = SolarSystemViewModel.Canvas;
         }
 
         private void Stop_Btn_Click(object sender, RoutedEventArgs e)
@@ -100,7 +53,7 @@ namespace AnimatedSolarSystem.View
             }
 
 			tokenSource = new CancellationTokenSource();
-			var task = Task.Run(() => MoveMyEllipse(tokenSource.Token));
+			var task = Task.Run(() => MoveMyPlanet(tokenSource.Token));
 		}
 
 		private void ScrollBar_Y_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -113,54 +66,30 @@ namespace AnimatedSolarSystem.View
 
 
 
-        private void MoveMyEllipse(CancellationToken token)
+        private void MoveMyPlanet(CancellationToken token)
         {
             while (!token.IsCancellationRequested)
             {
 
-                foreach (var planet in SolarSystem)
+                foreach (var planet in SolarSystemViewModel.SolarSystem)
                 {
-                    planet.Angle = planet.Angle + planet.Velocity;
-                    planet.X = planet.X0 + planet.Distance * Math.Sin(planet.Angle);
-                    planet.Y = planet.Y0 + planet.Distance * Math.Cos(planet.Angle) * SolarSystemViewModel.YPerspective;
+
+                    SolarSystemViewModel.MovePlanet(planet);
 
                     Invoke(() =>
                     {
-                        if (Math.Sin(planet.Angle) % 1.5807 <= -0.99)
-                        {
-                            if (!planet.Front)
-                            {
-                                planet.Front = true;
-                                FlipRender(planet.Shape);
-                            }
-                        }
-                        else if (Math.Sin(planet.Angle) % 1.5807 >= 0.99)
-                        {
-                            if (planet.Front)
-                            {
-                                planet.Front = false;
-                                FlipRender(planet.Shape);
-                            }
-                        }
-
+                        SolarSystemViewModel.FlipRender(planet);
                         planet.Shape.Margin = new Thickness(planet.X, planet.Y, 0, 0);
                     });
+
                 }
 
-                
-                Thread.Sleep(25);
+                    Thread.Sleep(25);
             }
         }
         private void Invoke(Action action)
         {
             Dispatcher?.Invoke(action);
-        }
-
-		public void FlipRender(Ellipse ellipse)
-        {
-
-            Canvas.SetZIndex(ellipse,-Canvas.GetZIndex(ellipse));
-
         }
 
         
--- AnimatedSolarSystem/ViewModel/SolarSystemViewModel.cs
+++ AnimatedSolarSystem/ViewModel/SolarSystemViewModel.cs
@@ -3,6 +3,7 @@ using AnimatedSolarSystem.View;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,6 +12,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 using System.Windows.Threading;
 using System.Xml.Linq;
 
@@ -21,14 +23,6 @@ namespace AnimatedSolarSystem.ViewModel
 
         public Canvas Canvas;
 
-        public void NewCanvas()
-        {
-            this.Canvas = new Canvas();
-			Canvas.Background = Brushes.Black;
-		}
-
-
-
 		private double yPerspective;
 
 		public double YPerspective
@@ -36,6 +30,87 @@ namespace AnimatedSolarSystem.ViewModel
 			get { return yPerspective; }
 			set { yPerspective = value; }
 		}
+
+
+        public void MovePlanet(SolarObject planet)
+        {
+            planet.Angle = planet.Angle + planet.Velocity;
+            planet.X = planet.X0 + planet.Distance * Math.Sin(planet.Angle);
+            planet.Y = planet.Y0 + planet.Distance * Math.Cos(planet.Angle) * this.YPerspective;
+        }
+
+
+        private List<SolarObject> solarSystem;
+
+        public List<SolarObject> SolarSystem
+        {
+            get { return solarSystem; }
+            set { solarSystem = value; }
+        }
+
+
+        public void CreateSolarSystem()
+        {
+            this.SolarSystem = new List<SolarObject>();
+
+            this.Canvas = new Canvas();
+            Canvas.Background = Brushes.Black;
+
+
+            Ellipse Sun = new Ellipse();
+            Sun.Fill = Brushes.Yellow;
+            Sun.Width = 200;
+            Sun.Height = 200;
+            Sun.Margin = new Thickness(707, 307, 0, 0);
+            Sun.StrokeThickness = 2;
+
+            Sun.Stroke = Brushes.White;
+
+            Canvas.Children.Add(Sun);
+
+            SolarSystem.Add(new SolarObject("Mercury", 75 + 50, 0.479, 10, Brushes.Gray));
+            SolarSystem.Add(new SolarObject("Venus", 100 + 50, 0.350, 20, Brushes.LightGoldenrodYellow));
+            SolarSystem.Add(new SolarObject("Earth", 130 + 50, 0.298, 20, Brushes.Blue));
+            SolarSystem.Add(new SolarObject("Mars", 155 + 50, 0.241, 10, Brushes.OrangeRed));
+            SolarSystem.Add(new SolarObject("Jupiter", 275 + 50, 0.131, 100, Brushes.Wheat));
+            SolarSystem.Add(new SolarObject("Saturn", 415 + 50, 0.97, 80, Brushes.SandyBrown));
+            SolarSystem.Add(new SolarObject("Uranus", 505 + 50, 0.68, 50, Brushes.WhiteSmoke));
+            SolarSystem.Add(new SolarObject("Neptun", 590 + 50, 0.54, 50, Brushes.LightBlue));
+
+
+            Canvas.SetZIndex(Sun, 0);
+
+            int count = 1;
+            foreach (SolarObject planet in SolarSystem)
+            {
+                Canvas.SetZIndex(planet.Shape, count);
+                count++;
+
+                Debug.WriteLine("Count: " + count);
+
+                Canvas.Children.Add(planet.Shape);
+            }
+        }
+
+        public void FlipRender(SolarObject planet)
+        {
+            if (Math.Sin(planet.Angle) % 1.5807 <= -0.99)
+            {
+                if (!planet.Front)
+                {
+                    planet.Front = true;
+                    Canvas.SetZIndex(planet.Shape, -Canvas.GetZIndex(planet.Shape));
+                }
+            }
+            else if (Math.Sin(planet.Angle) % 1.5807 >= 0.99)
+            {
+                if (planet.Front)
+                {
+                    planet.Front = false;
+                    Canvas.SetZIndex(planet.Shape, -Canvas.GetZIndex(planet.Shape));
+                }
+            }
+        }
 
 
     }
